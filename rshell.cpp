109ed832bd7b954fcd092118358afe9460a44493
TheBoop <davidxyding1@gmail.com>
2015-04-15 22:04:44 -0700

    Print $ in for loop

--- rshell.cpp
+++ rshell.cpp
@@ -1,7 +1,17 @@
-#inlcude <iostream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
+int main(int argc,char **argv){
+    string command;
+    cout << argv[1];
+
+    //While loop that repeats $ and cin command
+    while(command != "exit"){
+        cout << "$";
+        cin >> command;
+    }
+
     return 0;
 }
