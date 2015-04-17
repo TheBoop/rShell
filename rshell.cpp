7ef99f9a7d90ab6499060446cefcaf006864fb48
TheBoop <davidxyding1@gmail.com>
2015-04-17 01:09:49 -0700

    errors

--- rshell.cpp
+++ rshell.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 //Takes in commands and returns true or false
-bool checkCommand( string s){
-    
+bool checkCommand(char* comList ,char* argList){
+    int status;
     int pid = fork();
-    if(s == "exit"){
-        exit(0);
+    if(pid <= -1){
+        perror("Forking Error");
+        exit(1);
+    }else if (pid == 0){ //Children
+        if(execvp(comList ,argList)== -1){
+            perror("Executable or arg Error");
+            exit(1);
+        }
+    }else if (pid > 0) {//Parent
+        if(waitpid(pid, &status, 0) == -1)
+            perror("Wait Error");
     }
 }
 
