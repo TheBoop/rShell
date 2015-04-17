1c164d06dfde1be297e6fdd5e31c3936be9e495c
TheBoop <davidxyding1@gmail.com>
2015-04-17 00:45:58 -0700

    added some more stuff

--- rshell.cpp
+++ rshell.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 
 using namespace std;
 
+//Takes in commands and returns true or false
+bool checkCommand( string s){
+    
+    int pid = fork();
+    if(s == "exit"){
+        exit(0);
+    }
+}
+
 int main(int argc,char **argv){
     string command;
-    cout << argv[1];
+    cout << "begin shell" << endl;
 
     //While loop that repeats $ and cin command
     while(command != "exit"){
-        cout << "$";
+        cout << "$ ";
         cin >> command;
     }
 
